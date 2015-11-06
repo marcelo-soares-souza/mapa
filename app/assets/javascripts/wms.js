54d14f4203aa3de385361e9b3d7bc08f14139e09
Marcelo Soares Souza <marcelo@libertais.org>
2015-11-06 16:39:18 -0200

    Better WMS

--- app/assets/javascripts/wms.js
+++ app/assets/javascripts/wms.js
@@ -1,7 +1,14 @@
 // WMS Layers
+var url = "http://projetos.hexgis.com/geoserver/wms";
 
-var wms_ti = L.tileLayer.wms("http://projetos.hexgis.com/geoserver/wms", {
+// var wms_ti = L.tileLayer.wms("http://projetos.hexgis.com/geoserver/wms", {
+//      layers: 'HEX:terra_indigena',
+//      format: 'image/png',
+//      transparent: true,
+//      version: '1.3.0' });
+
+var wms_ti = L.tileLayer.betterWms(url, {
       layers: 'HEX:terra_indigena',
-      format: 'image/png',
-      transparent: true,
-      version: '1.3.0' });
+        transparent: true,
+        format: 'image/png'
+      });
